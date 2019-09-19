a28c18263f55216e9878a91aa603635e0423dc74
Bartłomiej Skoczeń <bartek_skoczen@protonmail.com>
2019-09-19 17:49:18 +0200

    Change project from Crystal to C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace lxdManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // TODO: Write this lol
+        }
+    }
+}
